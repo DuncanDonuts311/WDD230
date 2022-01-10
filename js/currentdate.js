9da498c86794193784e29c6ed716f67279ebcf11
DuncanDonuts311 <dun12024@byui.edu>
2022-01-10 16:58:09 -0500

    css

--- js/currentdate.js
+++
@@ -1,2 +0,0 @@
- const options = {weekday: 'long', day: 'numeric', month: 'short', year: 'numeric'};
- document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
