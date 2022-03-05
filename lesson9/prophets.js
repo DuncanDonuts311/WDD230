6d993886b6c526dd740f5459ab1c46eab5a561d1
DuncanDonuts311 <dun12024@byui.edu>
2022-03-05 12:18:31 -0500

    js changes

--- lesson9/prophets.js
+++ lesson9/prophets.js
@@ -1,49 +1,27 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
-    .then(function (response){
+      .then(function (response) {
         return response.json();
-    })
-    .then(function(jsonObject){
-        console.table(jsonObject);
-        const prophets=jsonObject['prophets'];
-        prophets.forEach(displayProphets);
-    });
-    function ordinal(number) {
-        let x = number % 10;
-        let y = number % 100;
-        if(x == 1 && y !== 11) {
-            return'${number}st';
-        }
-        else if (x == 2 && y !== 12) {
-            return'${number}nd';
-        }
-        else if(x == 3 && y !== 13) {
-            return'${number}rd';
-        }
-        return'${number}th';
-    }
-
-function displayProphets(prophet) {
-    let card = document.createElement('section');
-
-    let h2=document.createElement('h2');
-    let prophetNum = ordinal(prophet.order);
-    h2.textContent = '${prophet.name} ${prophet.lastname} ${prophetNum}';
-    card.appendChild(h2);
-
-    let p1 = document.createElement('p');
-    p1.textContent = 'Date of birth: ${prophet.birthdate}';
-    card.appendChild(p1);
-
-    let p2 = document.createElement('p');
-    p2.textContent = 'Place of birth: ${prophet.birthplace}';
-    card.appendChild(p2);
-
-    let image = document.createElement('img');
-    image.src = prophet.imageurl;
-    image.setAttribute('alt', prophet.order);
-    card.appendChild(image);
-
-    document.querySelector('div.cards').appendChild(card);
-}
+      })
+      .then(function (jsonObject) {
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const prophets = jsonObject['prophets'];
+        for (let i = 0; i < prophets.length; i++ ) {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let p1 = document.createElement('p');
+            let p2 = document.createElement('p');
+            let image = document.createElement('img');
+            h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
+            p1.textContent = `Birthday: ${prophets[i].birthdate}`;
+            p2.textContent = `Birthplace: ${prophets[i].birthplace}`;
+            image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', `Portait of ${prophets[i].name} ${prophets[i].lastname}`);
+            card.appendChild(h2);
+            card.appendChild(p1);
+            card.appendChild(p2);
+            card.appendChild(image);
+            document.querySelector('div.cards').appendChild(card);
+          }
+      });
